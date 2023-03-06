4bd9e3566791ebf0c8f47a39fc171e1a166ad5aa
Chitransh Varshney <114412898+Chitransh4Varshney@users.noreply.github.com>
2023-03-06 14:48:20 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,15 @@
+package Pattern;
+
+// Make full star square
+
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
